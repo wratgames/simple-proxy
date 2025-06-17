c4bd3af842eb9414c05e10d378c7c7161436dbb4
wratgames <ratgames325@gmail.com>
2025-06-17 08:06:43 -0600

    Update nitro.config.ts

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,14 +1,9 @@
 import { join } from "path";
-import pkg from "./package.json";
 
-// https://nitro.unjs.io/config
 export default defineNitroConfig({
-  compatibilityDate: "2025-04-20", // For Cloudflare Workers compatibility
-  srcDir: "./src",                 // Source directory for handlers
-  runtimeConfig: {
-    version: pkg.version           // Expose package version at runtime
-  },
+  compatibilityDate: "2025-04-20",
+  srcDir: "./src",
   alias: {
-    "@": join(__dirname, "src")    // Use "@/..." to reference files in src/
+    "@": join(__dirname, "src")
   }
 });
